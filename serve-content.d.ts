1dad2786bb70f2de3371b18cef97bc173c1016ef
Emilio Platzer <emilioplatzer@gmail.com>
2020-11-08 11:13:51 -0300

    Prueba de cambiar el .d.ts

--- serve-content.d.ts
+++ serve-content.d.ts
@@ -1,19 +1,22 @@
 import * as express from 'express';
 
-export declare function serveContent(root: string, options: ServeContentOptions): express.Handler;
-export interface ServeContentOptions {
-    allowedExts?: string[];
-    allowAllExts?: boolean;
-    excludeExts?: string[];
-    cacheControl?: boolean;
-    dotfiles?: string;
-    etag?: boolean;
-    extensions?: string[] | false;
-    fallthrough?: boolean;
-    immutable?: boolean;
-    index?: boolean | string | string[];
-    lastModified?: boolean;
-    maxAge?: number | string;
-    redirect?: boolean;
-    setHeaders?: (res: express.Response, path: string, stat: any) => any;
+declare function serveContent(root: string, options: serveContent.ServeContentOptions): express.Handler;
+declare namespace serveContent {
+    export interface ServeContentOptions {
+        allowedExts?: string[];
+        allowAllExts?: boolean;
+        excludeExts?: string[];
+        cacheControl?: boolean;
+        dotfiles?: string;
+        etag?: boolean;
+        extensions?: string[] | false;
+        fallthrough?: boolean;
+        immutable?: boolean;
+        index?: boolean | string | string[];
+        lastModified?: boolean;
+        maxAge?: number | string;
+        redirect?: boolean;
+        setHeaders?: (res: express.Response, path: string, stat: any) => any;
+    }
+    function serveContent(root: string, options: ServeContentOptions): express.Handler;
 }
